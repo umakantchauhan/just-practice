cb2982ea1a70cbe159055833d43793b5ffa5434f
umakantchauhan <ukchauhan369@gmail.com>
2025-08-07 22:19:13 +0530

    just revising

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import LudoBoard from './LudoBoard'
-import Form from "./Form"
-import CommentsForm from '../CommentsForm'
 
 function App() {
 
