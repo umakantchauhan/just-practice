10cab21492116427b4140a3f64c3cc0a5df1f416
umakantchauhan <ukchauhan369@gmail.com>
2025-08-12 21:57:56 +0530

    made user login with UI

--- CommentsForm.jsx
+++ CommentsForm.jsx
@@ -9,7 +9,12 @@ export default function CommentsForm(){
                     <textarea name="" id="">Remarks</textarea>
                     <br /><br />
                     <input placeholder="rating" type="number" min={1} max={5} />
+
+                    
                     <br /><br />
+
+
+                    
                     <button>Add comments</button>
                     
                 </form>
