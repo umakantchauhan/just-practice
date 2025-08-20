85f85758afc45c83094ab15314ed5dbbbf436afe
umakantchauhan <ukchauhan369@gmail.com>
2025-08-20 22:53:37 +0530

    m

--- src/Form.jsx
+++ src/Form.jsx
@@ -5,19 +5,12 @@ export default function Form(){
     let handleNameChange =(event)=>{
         setFullName(event.target.value);
     }
-
-
-     
     return (
         <form action="">
             <label htmlFor="username">Full Name</label>
             <input placeholder="enter full name" type="text" value={fullName} onChange={handleNameChange} id="username" />
             <button>Submit</button>
-            <frame />
-
-
-            
+            <frame /> 
         </form>
     )
-    
 }
